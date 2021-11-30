0c8f14fbe758916c6ff75e283a9d6abd2fe68d47
Shania Tsamara <shaniatsamara@gmail.com>
2021-11-30 15:37:03 +0800

    added the dashboard view, and the needed variables in the controller. changed the navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_024718) do
+ActiveRecord::Schema.define(version: 2021_11_30_072642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_024718) do
     t.integer "total_price"
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.string "status", default: "Pending"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["family_id"], name: "index_bookings_on_family_id"
