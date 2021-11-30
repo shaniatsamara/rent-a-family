4536faefde8130b1286673cf666a19c04f9acfb2
Brian Koekkoek <bjkoekkoek@hotmail.com>
2021-11-30 16:42:10 +0800

    Modified file for authenticating user access

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [:home, :index, :show]
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: %i[home index show]
 
   def home
     # landing page
