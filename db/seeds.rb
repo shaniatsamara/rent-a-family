d9677ea740c9ee87bf7afa6d4db66c4dc8e57878
Shania Tsamara <shaniatsamara@gmail.com>
2021-11-30 12:37:50 +0800

    created some seeds, changed the collection in bookings model to STATUSES, and added dependent destroy on the user model for many families

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Booking.destroy_all
+Family.destroy_all
+User.destroy_all
+
+10.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.street_name,
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+  user.save!
+end
+
+
+10.times do
+  family = Family.new(
+    race: Family::ETHNICITY.sample,
+    title: Faker::TvShows::FamilyGuy.character,
+    description: Faker::TvShows::FamilyGuy.quote,
+    location: Faker::Address.city,
+    price: rand(1..10),
+    adult_male: rand(0..2),
+    adult_female: rand(0..2),
+    child_male: rand(0..2),
+    child_female: rand(0..2),
+    senior_male: rand(0..2),
+    senior_female: rand(0..2),
+    user: User.all.sample
+  )
+  family.save!
+end
+
+10.times do
+  start_date = Date.today + rand(-10..20)
+  end_date = start_date + rand(20..30)
+  family = Family.all.sample
+  booking = Booking.new(
+    start_date: start_date,
+    end_date: end_date,
+    payment_method: Booking::PAYMENT_METHODS.sample,
+    total_price: (end_date - start_date) * family.price,
+    user: User.all.sample,
+    status: Booking::STATUSES.sample,
+    family: family
+  )
+  booking.save!
+end
