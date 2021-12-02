29de77ab1c49d9f5fc1df6ecfd9c4008ba9f5e89
Shania Tsamara <shaniatsamara@gmail.com>
2021-12-02 16:35:31 +0800

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :families, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_one_attached :avatar
+
+  validates :email, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_030621) do
+ActiveRecord::Schema.define(version: 2021_12_02_082950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,12 +60,12 @@ ActiveRecord::Schema.define(version: 2021_12_02_030621) do
   create_table "families", force: :cascade do |t|
     t.string "ethnicity"
     t.integer "price"
-    t.integer "adult_male"
-    t.integer "adult_female"
-    t.integer "child_male"
-    t.integer "child_female"
-    t.integer "senior_male"
-    t.integer "senior_female"
+    t.integer "adult_male", default: 0
+    t.integer "adult_female", default: 0
+    t.integer "child_male", default: 0
+    t.integer "child_female", default: 0
+    t.integer "senior_male", default: 0
+    t.integer "senior_female", default: 0
     t.text "description"
     t.string "location"
     t.bigint "user_id", null: false
