b1c130a0fe5d39333835c497be9ea4466e2683bd
Brian Koekkoek <bjkoekkoek@hotmail.com>
2021-12-03 14:59:34 +0800

    Styled showpage bit more, minor other adjustments

--- app/javascript/packs/plugins/init_typed.js
+++ app/javascript/packs/plugins/init_typed.js
@@ -4,7 +4,7 @@ const initTyped = () => {
 const checker = document.getElementById("family");
   if (checker) {
     const options = {
-      strings: ['cool dad!', 'non-strict mom!', 'non-annoying sister!', 'fun brother!', 'fun but wierd cousin!'],
+      strings: ['cool dad!', 'non-strict mom!', 'non-annoying sister!', 'fun brother!', 'fun but weird cousin!'],
       typeSpeed: 50,
       loop: true,
       smartBackspace: true
